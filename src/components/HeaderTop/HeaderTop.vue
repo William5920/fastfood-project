c72be3cd79c40895baf0fd2f2d39c07469ef01ce
William5920 <1101044179@qq.com>
2019-09-22 18:00:50 +0800

    初始化外卖项目

---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,72 @@
+<template>
+	<header class="header">
+		<slot name="left"></slot>
+		<span class="header-title">
+			<span class="header-title-text ellipsis">{{title}}</span>
+		</span>
+		<slot name="right"></slot>
+	</header>
+</template>
+
+<script>
+	export default {
+		props: {
+			title: String
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.header {
+		background-color: #02a774;
+		width: 100%;
+		height: 45px;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 100;
+
+		.header-search {
+			position: absolute;
+			left: 15px;
+			top: 50%;
+			transform: translateY(-50%);
+			width: 10%;
+			height: 50%;
+
+			.icon-icon_sousuo {
+				font-size: 25px;
+				color: #fff;
+			}
+		}
+
+		.header-title {
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%, -50%);
+			width: 50%;
+			color: #fff;
+			text-align: center;
+
+			.header-title-text {
+				font-size: 20px;
+				color: #fff;
+				display: block;
+			}
+		}
+
+		.header-login {
+			position: absolute;
+			right: 15px;
+			top: 50%;
+			transform: translateY(-50%);
+			font-size: 14px;
+			color: #fff;
+
+			.header-login-text {
+				color: #fff;
+			}
+		}
+	}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import MSite from './views/MSite/MSite.vue'
+import Order from './views/Order/Order.vue'
+import Profile from './views/Profile/Profile.vue'
+import Search from './views/Search/Search.vue'
+import Login from './views/Login/Login.vue'
 
 Vue.use(Router)
 
@@ -9,17 +13,48 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/msite',
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
